b6686cd35a656fd4289073d18ada83aeef64c21d
David Muto (pseudomuto) <david.muto@gmail.com>
2018-11-18 16:45:36 -0500

    Ensure HTTP status code is included in log messages

--- http/middleware.go
+++ http/middleware.go
@@ -7,17 +7,29 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type statusResponseWriter struct {
+	http.ResponseWriter
+	status int
+}
+
+func (w *statusResponseWriter) WriteHeader(code int) {
+	w.status = code
+	w.ResponseWriter.WriteHeader(code)
+}
+
 // WithLogging wraps the supplied handler to handle request logging
 func WithLogging(logger log.FieldLogger, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		startTime := time.Now()
-		h.ServeHTTP(w, r)
+		sw := &statusResponseWriter{w, http.StatusOK}
+		h.ServeHTTP(sw, r)
 
 		duration := time.Since(startTime)
 		logger.WithFields(log.Fields{
 			"dur":    duration,
 			"method": r.Method,
 			"path":   r.URL.Path,
+			"status": sw.status,
 		}).Info("processed request")
 	})
 }
--- http/middleware_test.go
+++ http/middleware_test.go
@@ -14,9 +14,11 @@ func TestWithLogging(t *testing.T) {
 	tests := []struct {
 		path   string
 		method string
+		status int
 	}{
-		{path: "/some/path", method: "GET"},
-		{path: "/thing/create", method: "POST"},
+		{path: "/some/path", method: "GET", status: 200},
+		{path: "/thing/create", method: "POST", status: 201},
+		{path: "/thing/unknown", method: "DELETE", status: 404},
 	}
 
 	for _, test := range tests {
@@ -24,11 +26,15 @@ func TestWithLogging(t *testing.T) {
 		r := httptest.NewRequest(test.method, test.path, nil)
 		logger, hook := logTest.NewNullLogger()
 
-		h := http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {})
+		h := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
+			w.WriteHeader(test.status)
+		})
+
 		WithLogging(logger, h).ServeHTTP(w, r)
 
 		entry := hook.LastEntry()
 		assert.Equal(t, test.method, entry.Data["method"])
 		assert.Equal(t, test.path, entry.Data["path"])
+		assert.Equal(t, test.status, entry.Data["status"])
 	}
 }
